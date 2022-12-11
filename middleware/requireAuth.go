6f31716b7b22c21cd8dfea67652e2a349fd8f1a1
dhammikod <63945345+dhammikod@users.noreply.github.com>
2022-12-12 01:04:32 +0700

    auth done

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -2,18 +2,55 @@ package middleware
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	"time"
 
+	"github.com/dhammikod/cruduser/initializers"
+	"github.com/dhammikod/cruduser/models"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func RequireAuth(c *gin.Context) {
-	fmt.Println("in middleware")
 	//get the cookie ff req
-	//decode/validate cookie
-	//check expired
-	//find user with token
-	//attach to req
-	//continue
+	tokenString, err := c.Cookie("authorization")
 
-	c.Next()
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	//decode/validate cookie
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(os.Getenv("SECRET")), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		//check expired
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		//find user with token
+		var user models.User
+		initializers.DB.First(&user, claims["sub"])
+
+		if user.ID == 0 {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		//attach to req
+		c.Set("user", user)
+		//continue
+		c.Next()
+		fmt.Println(claims["foo"], claims["nbf"])
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
 }
