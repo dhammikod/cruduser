1aadd177ed6cf07f6dd67fe6e3eb0f6c676c8dc2
dhammikod <63945345+dhammikod@users.noreply.github.com>
2022-12-26 14:01:12 +0700

    seeder

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/dhammikod/cruduser/initializers"
 	"github.com/dhammikod/cruduser/models"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func init() {
@@ -15,4 +19,56 @@ func main() {
 	initializers.DB.AutoMigrate(&models.Resep{})
 	initializers.DB.AutoMigrate(&models.Bahan{})
 	initializers.DB.AutoMigrate(&models.Listbahan{})
+
+	//creating Users
+	hash, err := bcrypt.GenerateFromPassword([]byte("password123"), 10)
+	if err != nil {
+		fmt.Println("ada error")
+	}
+	initializers.DB.Create(&models.User{Name: "Dhammiko", Email: "Dhammiko@gmail.com", Password: string(hash), No_telp: "082233123", Profile_picture: "profle picture", Notification: false})
+	initializers.DB.Create(&models.User{Name: "hagen", Email: "hagen@gmail.com", Password: string(hash), No_telp: "324523212", Profile_picture: "profle picture", Notification: false})
+	initializers.DB.Create(&models.User{Name: "siapa", Email: "siapa1@gmail.com", Password: string(hash), No_telp: "812361273", Profile_picture: "profle picture", Notification: false})
+	initializers.DB.Create(&models.User{Name: "ludwig", Email: "ludwig@gmail.com", Password: string(hash), No_telp: "657322311", Profile_picture: "profle picture", Notification: false})
+	initializers.DB.Create(&models.User{Name: "magnus", Email: "magnus@gmail.com", Password: string(hash), No_telp: "856362211", Profile_picture: "profle picture", Notification: false})
+
+	//creating reseps
+	initializers.DB.Create(&models.Resep{Created_by: 1, Judul: "nasi goreng", Foto: "fotonasigoreng", Video: "videomasaknasi", Portionsize: 3, Description: "nasi goreng khusus mahasiswa on budget", Rating: 3, Jumlahrating: 0})
+	initializers.DB.Create(&models.Resep{Created_by: 2, Judul: "bebek bakar", Foto: "foto bebek bakar", Video: "videomasakbebek", Portionsize: 1, Description: "aves mewah", Rating: 5, Jumlahrating: 0})
+	initializers.DB.Create(&models.Resep{Created_by: 1, Judul: "mie goreng", Foto: "fotomiegoreng", Video: "videomasakmie", Portionsize: 1, Description: "mie goreng khusus mahasiswa on budget", Rating: 1, Jumlahrating: 0})
+	initializers.DB.Create(&models.Resep{Created_by: 3, Judul: "indomie mewah", Foto: "foto indomie", Video: "videomasakmie", Portionsize: 1, Description: "mie goreng indomie bernutrisi", Rating: 5, Jumlahrating: 0})
+	initializers.DB.Create(&models.Resep{Created_by: 4, Judul: "fu yung hai", Foto: "foto telur", Video: "videomasaktelur", Portionsize: 2, Description: "chineese food yang mudah dimasak", Rating: 4, Jumlahrating: 0})
+
+	//create bahan
+	initializers.DB.Create(&models.Bahan{Namabahan: "kentang"})
+	initializers.DB.Create(&models.Bahan{Namabahan: "nasi"})
+	initializers.DB.Create(&models.Bahan{Namabahan: "mie"})
+	initializers.DB.Create(&models.Bahan{Namabahan: "bebek"})
+	initializers.DB.Create(&models.Bahan{Namabahan: "indomie"})
+	initializers.DB.Create(&models.Bahan{Namabahan: "telur"})
+	initializers.DB.Create(&models.Bahan{Namabahan: "sosis"})
+
+	//create list bahan
+	initializers.DB.Create(&models.Listbahan{Resep_id: 1, Bahan_id: 2, Jumlahbahan: 2})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 1, Bahan_id: 1, Jumlahbahan: 1})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 1, Bahan_id: 6, Jumlahbahan: 2})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 1, Bahan_id: 7, Jumlahbahan: 2})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 2, Bahan_id: 4, Jumlahbahan: 1})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 3, Bahan_id: 3, Jumlahbahan: 2})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 3, Bahan_id: 6, Jumlahbahan: 2})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 3, Bahan_id: 7, Jumlahbahan: 2})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 4, Bahan_id: 5, Jumlahbahan: 2})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 4, Bahan_id: 6, Jumlahbahan: 1})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 4, Bahan_id: 7, Jumlahbahan: 1})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 5, Bahan_id: 1, Jumlahbahan: 2})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 5, Bahan_id: 6, Jumlahbahan: 2})
+	initializers.DB.Create(&models.Listbahan{Resep_id: 5, Bahan_id: 7, Jumlahbahan: 2})
+
+	//create saved recipe
+	initializers.DB.Table("saved_recipes").Create(map[string]interface{}{"resep_id": 1, "user_id": 4})
+	initializers.DB.Table("saved_recipes").Create(map[string]interface{}{"resep_id": 1, "user_id": 5})
+	initializers.DB.Table("saved_recipes").Create(map[string]interface{}{"resep_id": 2, "user_id": 5})
+	initializers.DB.Table("saved_recipes").Create(map[string]interface{}{"resep_id": 3, "user_id": 5})
+	initializers.DB.Table("saved_recipes").Create(map[string]interface{}{"resep_id": 4, "user_id": 5})
+	initializers.DB.Table("saved_recipes").Create(map[string]interface{}{"resep_id": 4, "user_id": 1})
+
 }
